7b5b2096da37f671e118814066a1238833ddc475
Abhinit Tripathi <114798873+AbhinitTripathi@users.noreply.github.com>
2025-08-13 05:41:40 +0000

    keyboard onclick and keydown functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
-import { useState } from 'react'
-import Header from './components/Header.jsx'
-import Status from './components/Status.jsx'
-import LanguagesChips from './components/LanguagesChips.jsx'
-import Word from './components/Word.jsx'
-import Keyboard from './components/Keyboard.jsx'
-
+import { useState } from "react";
+import Header from "./components/Header.jsx";
+import Status from "./components/Status.jsx";
+import LanguagesChips from "./components/LanguagesChips.jsx";
+import Word from "./components/Word.jsx";
+import Keyboard from "./components/Keyboard.jsx";
 
 export default function App() {
-    const[currentWord, setCurrentWord] = useState("react");
-    const letterElements = currentWord.split('').map((letter, i) => <span key={i}>{letter.toLocaleUpperCase()}</span>)
+    // Shared with WORD component
+    const [currentWord, setCurrentWord] = useState("react");
+    const letterElements = currentWord
+        .split("")
+        .map((letter, i) => <span key={i}>{letter.toLocaleUpperCase()}</span>);
 
-    
+    const [guessedLetters, setGuessedLetters] = useState([]);
+
     return (
         <main>
-            <Header/>
-            <Status/>
-            <LanguagesChips/>
+            <Header />
+            <Status />
+            <LanguagesChips />
             <Word letterElements={letterElements} />
-            <Keyboard/>
+            <Keyboard
+                guessedLetters={guessedLetters}
+                setGuessedLetters={setGuessedLetters}
+            />
 
             <button className="new-game">New Game</button>
         </main>
-        
-    )
+    );
 }
--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,16 +1,56 @@
-export default function Keyboard() {
-    const layout = [
-        "QWERTYUIOP",
-        "ASDFGHJKL",
-        "ZXCVBNM"
-    ];
+import { useEffect, useRef } from "react";
+export default function Keyboard(props) {
+    const layout = ["QWERTYUIOP", "ASDFGHJKL", "ZXCVBNM"];
+
+    // useRef so the the useEffect knows the latest guessedLetters for duplicate checks
+    const guessedLettersRef = useRef(props.guessedLetters);
+    useEffect(() => {
+        guessedLettersRef.current = props.guessedLetters;
+    }, [props.guessedLetters]);
+
+    // function for both click and keydown events
+    function addGuessedLetters(e) {
+        e.stopPropagation();
+        const letter = e.key ? e.key.toUpperCase() : e.target.innerText;
+
+        if (
+            guessedLettersRef.current.includes(letter) ||
+            letter.charCodeAt(0) < 65 ||
+            letter.charCodeAt(0) > 90 ||
+            letter.length > 1
+        )
+            return;
+
+        props.setGuessedLetters(prev => [...prev, letter]);
+    }
+
+    // Add event listener for keydown when the component mounts
+    useEffect(() => {
+        window.addEventListener("keydown", addGuessedLetters);
+        return () => window.removeEventListener("keydown", addGuessedLetters);
+    }, []);
+    // Update guessedLetters from keydown event
+    useEffect(() => {
+        console.log(props.guessedLetters);
+    }, [props.guessedLetters]);
+
+    useEffect(() => {
+        window.addEventListener("keydown", addGuessedLetters);
+        return () => window.removeEventListener("keydown", addGuessedLetters);
+    }, []);
 
     return (
         <section className="keyboard">
             {layout.map((row, rowIndex) => (
                 <div className="keyboard-row" key={rowIndex}>
-                    {row.split('').map((alphabet, i) => (
-                        <button key={i} className="key">{alphabet}</button>
+                    {row.split("").map((alphabet, i) => (
+                        <button
+                            key={i}
+                            onClick={addGuessedLetters}
+                            className="key"
+                        >
+                            {alphabet}
+                        </button>
                     ))}
                 </div>
             ))}
