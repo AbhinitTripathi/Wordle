4b95921159594ae342f98915b5c478d58a1a793c
Abhinit Tripathi <114798873+AbhinitTripathi@users.noreply.github.com>
2025-08-14 09:00:42 +0000

    main game done

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Word from "./components/Word.jsx";
 import Keyboard from "./components/Keyboard.jsx";
 import { languages } from "./languages.js";
 import getRandomWord from "./wordsToGuess.js";
+import { useWindowSize } from "react-use";
+import Confetti from "react-confetti";
 
 export default function App() {
     // Shared with WORD & KEYBOARD component
@@ -32,7 +34,11 @@ export default function App() {
 
     // Check if last guessed letter in correct or not for farewell message
     const lastGuessedLetter = guessedLetters[guessedLetters.length - 1];
-    const isLastGuessedIncorrect = lastGuessedLetter && !currentWord.includes(lastGuessedLetter.toLowerCase());
+    const isLastGuessedIncorrect =
+        lastGuessedLetter &&
+        !currentWord.includes(lastGuessedLetter.toLowerCase());
+
+    const { width, height } = useWindowSize();
 
     return (
         <main>
@@ -44,9 +50,7 @@ export default function App() {
                 isLastGuessedIncorrect={isLastGuessedIncorrect}
                 wrongGuessCount={wrongGuessCount}
             />
-            <LanguagesChips
-                wrongGuessCount={wrongGuessCount}
-            />
+            <LanguagesChips wrongGuessCount={wrongGuessCount} />
             <Word
                 currentWord={currentWord}
                 letterElements={letterElements}
@@ -61,10 +65,30 @@ export default function App() {
                 isGameOver={isGameOver}
             />
 
-            {isGameOver ? <button onClick={() => {
-                setCurrentWord(getRandomWord());
-                setGuessedLetters([]);
-            }} className="new-game">New Game</button> : null}
+            {isGameOver ? (
+                <button
+                    onClick={() => {
+                        setCurrentWord(getRandomWord());
+                        setGuessedLetters([]);
+                    }}
+                    className="new-game"
+                >
+                    New Game
+                </button>
+            ) : null}
+
+            {isGameWon ? (
+                <Confetti
+                    width={width}
+                    height={height}
+                    numberOfPieces={500}
+                    gravity={0.4}
+                    initialVelocityY={20}
+                    initialVelocityX={15}
+                    recycle={false}
+                    confettiSource={{ x: 0, y: 0, w: width, h: height }}
+                />
+            ) : null}
         </main>
     );
 }
